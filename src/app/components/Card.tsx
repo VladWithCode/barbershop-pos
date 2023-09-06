6c8a5a13f0e190a1e09eb237e42725f38f4b09d2
vladwithcode <vladwithcode@gmail.com>
2023-09-06 16:11:40 -0600

    Change styles (might delete later)

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -6,11 +6,7 @@ function Card({
 	children,
 }: { className?: string } & React.PropsWithChildren) {
 	return (
-		<div
-			className={getClassName(
-				'bg-neutral-950 px-6 py-4 border-r-2 border-zinc-900',
-				className
-			)}>
+		<div className={getClassName('bg-neutral-950 px-6 py-4', className)}>
 			{children}
 		</div>
 	);
