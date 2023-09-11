80edde55b04110581df7128116132f8f20d92a1a
vladwithcode <vladwithcode@gmail.com>
2023-09-10 20:00:01 -0600

    Change bg color

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,5 +1,6 @@
 const globals = {
-	API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3000',
+	API_BASE_URL:
+		process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
 };
 
 export default globals;
