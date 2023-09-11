921cf50227a540e12ebd9ca148c55f51bd5e3663
vladwithcode <vladwithcode@gmail.com>
2023-09-10 21:05:35 -0600

    Change text-color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import ActionsList from './components/Quick Actions/ActionsList';
 
 export default function Home() {
 	return (
-		<div className="overflow-hidden overflow-y-auto w-screen page-height bg-zinc-950">
-			<h2 className="py-3 px-2 text-sm font-bold text-zinc-50">
+		<div className="overflow-hidden overflow-y-auto w-screen h-page">
+			<h2 className="py-3 px-2 text-xs font-bold text-zinc-950">
 				Bienvenido, usuario
 			</h2>
 
