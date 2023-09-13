091e8a27d0d164392524517cff9cdefb393da8b2
vladwithcode <vladwithcode@gmail.com>
2023-09-12 18:55:50 -0600

    Dont use useQuery

--- src/app/productos/_components/ProductList.tsx
+++ src/app/productos/_components/ProductList.tsx
@@ -1,10 +1,47 @@
 'use client';
 import { useQuery } from '@tanstack/react-query';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 import globals from '@/app/globals';
 import Loading from '@/app/_components/Loading/Loading';
 import useDebounce from '@/app/_hooks/useDebounce';
+
+const useProducts = (options?: {
+	search?: string;
+	limit?: number;
+	skip?: number;
+}) => {
+	const [products, setProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	const fetchProducts = async () => {
+		const response = await fetch(
+			globals.API_BASE_URL +
+				`/products?limit=${options?.limit || 16}&skip=${
+					options?.skip || 0
+				}&search=${options?.search || ''}`,
+			{ mode: 'no-cors' }
+		);
+		const data = await response.json();
+
+		return data;
+	};
+
+	useEffect(() => {
+		fetchProducts()
+			.then(data => {
+				setProducts(data);
+				setIsLoading(false);
+			})
+			.catch(err => {
+				setError(err);
+				setIsLoading(false);
+			});
+	}, []);
+
+	return { products, isLoading, error };
+};
 
 function ProductList() {
 	const [search, setSearch] = useState('');
@@ -12,7 +49,8 @@ function ProductList() {
 
 	useDebounce(() => setDebouncedSearch(search), 700, [search]);
 
-	const { data, error, isLoading, isError } = useQuery(
+	const { products, isLoading, error } = useProducts();
+	/* 	const { data, error, isLoading, isError } = useQuery(
 		['products', debouncedSearch],
 		async () => {
 			const response = await fetch(
@@ -24,7 +62,7 @@ function ProductList() {
 
 			return data;
 		}
-	);
+	); */
 
 	return (
 		<div className="w-full h-full px-8 pt-2 overflow-hidden overflow-x-auto mb-6">
@@ -44,13 +82,13 @@ function ProductList() {
 
 			{isLoading ? (
 				<Loading />
-			) : isError && error ? (
+			) : error ? (
 				<p>{(error as any).message}</p>
 			) : null}
 
-			{data ? (
+			{products ? (
 				<div className="grid grid-cols-2 max-h-[500px] lg:grid lg:flex-wrap gap-3 xl:gap-4 mx-auto overflow-y-auto">
-					{data?.map((p: any) => (
+					{products.map((p: any) => (
 						<ProductCard
 							_id={p._id}
 							name={p.name}
