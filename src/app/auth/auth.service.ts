b0e85c69e13b1a9931ecd3f07e21eca2ce262dd5
vladwithcode <vladwithcode@gmail.com>
2023-09-17 21:15:54 -0600

    Add ValidateLogin fn & use AxiosInstance

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,25 +1,25 @@
 import { useMutation } from '@tanstack/react-query';
-import globals from '../globals';
-
-const baseUrl = globals.API_BASE_URL + '/auth';
+import AxiosInstance from '../_utils/api';
 
 async function loginMutateFn(loginData: {
 	username: string;
 	password: string;
 }) {
-	const response = await fetch(baseUrl + '/login', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(loginData),
-	});
+	const response = await AxiosInstance.post('/auth/login', loginData);
 
-	const data = await response.json();
+	if (response.status !== 201) throw new Error(response.data.message);
 
-	if (!response.ok) throw new Error(data.message);
-
-	return data;
+	return response.data;
 }
 
 export const useLogin = () => useMutation(['login'], loginMutateFn);
+
+export async function validateLogin(token?: string) {
+	const response = await AxiosInstance.get('/auth/validate', {
+		headers: { Authorization: `Bearer ${token}` },
+	});
+
+	if (response.status !== 200) throw new Error(response.data.message);
+
+	return response.data;
+}
