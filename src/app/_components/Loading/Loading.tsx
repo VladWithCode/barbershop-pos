81f22b6cbeba68901a4123b8cc5dee06c5c66634
vladwithcode <vladwithcode@gmail.com>
2023-09-17 18:40:14 -0600

    Add Loading component

--- src/app/_components/Loading/Loading.tsx
+++ src/app/_components/Loading/Loading.tsx
@@ -5,7 +5,7 @@ function Loading() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full h-full gap-y-3">
 			<Spinner />
-			<p className="text-zinc-50 uppercase font-bold">Cargando...</p>
+			<p className="uppercase font-bold">Cargando...</p>
 		</div>
 	);
 }
