269005b753cacad13d78b7f807cf0bbe414ec15d
vladwithcode <vladwithcode@gmail.com>
2023-09-19 12:41:23 -0600

    Add default return for NaN vaues

--- src/app/_utils/helpers.ts
+++ src/app/_utils/helpers.ts
@@ -2,7 +2,9 @@ export function getClassName(...classList: (string | boolean | undefined)[]) {
 	return classList.filter(Boolean).join(' ');
 }
 
-export function numberToPrice(n: number) {
+export function numberToPrice(n: number | string) {
+	if (Number.isNaN(Number(n))) return '0.00';
+
 	return n.toLocaleString('es-MX', {
 		style: 'currency',
 		currency: 'MXN',
