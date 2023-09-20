07c928ac55633890b156b1f2336222110eff9d77
vladwithcode <vladwithcode@gmail.com>
2023-09-19 18:14:09 -0600

    Change map products to item interface

--- src/app/ventas/_services/sale.service.ts
+++ src/app/ventas/_services/sale.service.ts
@@ -1,22 +1,40 @@
 import AxiosInstance from '@/app/_utils/api';
-import { PaymentMethod, PaymentType } from '../_stores/useCreateSaleStore';
+import {
+	PaymentMethod,
+	PaymentType,
+	Product,
+} from '../_stores/useCreateSaleStore';
 import { useMutation } from '@tanstack/react-query';
 
 export type SaleData = {
 	customer: string;
 	seller: string;
-	products: Record<string, number>[];
+	products: Product[];
 	payment_type: PaymentType;
 	payment_method: PaymentMethod;
 	deposit: number;
-	total: number;
 	commission: number;
 	installment?: number;
-	next_payment_date?: Date;
+	next_payment_date?: string;
 };
 
 export async function createSale(saleData: SaleData) {
-	const response = await AxiosInstance.post('/sales', saleData);
+	const priceKey =
+		saleData.payment_type === 'cash'
+			? 'sell_price_cash'
+			: 'sell_price_credit';
+	const response = await AxiosInstance.post('/sales', {
+		...saleData,
+		items: saleData.products.map(p => {
+			return {
+				product: p._id,
+				stock_entry_id: p.stock_entry_id,
+				quantity: p.qty,
+				total: p[priceKey] * p.qty,
+				sale_price: p.sale_price || 0,
+			};
+		}),
+	});
 
 	return response.data;
 }
