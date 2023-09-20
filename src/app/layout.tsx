82724fb1459d0e730cf7985f21691fa5d2165b7b
vladwithcode <vladwithcode@gmail.com>
2023-09-19 18:15:10 -0600

    Add Toast wrapper to global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import Sidebar from './_components/Sidebar/Sidebar';
 import GlobalQueryProvider from './_components/GlobalQueryProvider';
+import ToastWrapper from './_components/Toast/Toast';
 
 const font = Poppins({
 	weight: ['300', '400', '500', '700'],
@@ -26,6 +27,7 @@ export default function RootLayout({
 					<GlobalQueryProvider>
 						<Sidebar />
 						{children}
+						<ToastWrapper />
 					</GlobalQueryProvider>
 				</div>
 			</body>
