7c23d80848a10d4c5260b4dfc79a567b5171cedd
VladWithCode <vladwithcode@gmail.com>
2023-09-29 14:03:37 -0600

    Add separate mutations for update & delete

--- src/app/clientes/_hooks/useCustomers.ts
+++ src/app/clientes/_hooks/useCustomers.ts
@@ -1,18 +1,51 @@
+import { queryClient } from '@/app/_components/GlobalQueryProvider';
 import {
+	CustomerDoc,
 	createCustomer,
+	deleteCustomer,
 	getCustomerById,
 	getCustomers,
+	updateCustomer,
 } from '../_services/CustomerService';
 import { useMutation, useQuery } from '@tanstack/react-query';
 
 export function useCustomers() {
-	return useQuery(['customers', 'get'], getCustomers);
+	return useQuery<CustomerDoc[], { message: string; error?: any }>(
+		['customers', 'get'],
+		getCustomers
+	);
 }
 
 export function useCustomerById(id: string) {
-	return useQuery(['customers', 'get', id], () => getCustomerById(id));
+	return useQuery<CustomerDoc, { message: string; error?: any }>(
+		['customers', 'get', id],
+		() => getCustomerById(id)
+	);
+}
+
+export function useUpdateCustomer(id: string) {
+	return useMutation(
+		['customers', 'update', id],
+		(customerData: CustomerDoc) =>
+			updateCustomer(id, { ...customerData, _id: id }),
+		{
+			onSuccess: () => {
+				// Invalidate and refetch
+				queryClient.invalidateQueries(['customers', 'get', id]);
+			},
+		}
+	);
 }
 
 export function useMutateCustomer() {
 	return useMutation(['customers', 'create'], createCustomer);
+}
+
+export function useDeleteCustomer(id: string) {
+	return useMutation(['customers', 'delete', id], () => deleteCustomer(id), {
+		onSuccess: () => {
+			// Invalidate and refetch
+			queryClient.invalidateQueries(['customers', 'get']);
+		},
+	});
 }
