db7dbad3cf4459ad47ba8f8e9487d516d517321a
VladWithCode <vladwithcode@gmail.com>
2023-09-29 14:00:37 -0600

    Change layout spacing

--- src/app/clientes/page.tsx
+++ src/app/clientes/page.tsx
@@ -5,20 +5,19 @@ import CustomerListing from './_components/CustomerListing';
 export default function Clientes() {
 	return (
 		<div className="h-page py-2">
-			<div className="h-full max-w-screen-xl grid grid-cols-12 auto-rows-fr justify-between mx-auto gap-x-2 overflow-hidden">
+			<div className="h-full max-w-screen-xl grid grid-cols-12 auto-rows-fr justify-between mx-auto gap-x-4 overflow-hidden">
 				{/* Customer List */}
-				<div className="col-span-6 px-2 py-4">
+				<div className="col-span-8 px-2 py-4">
 					<h1 className="text-xl font-medium">Clientes</h1>
 					<p className="text-zinc-500">
 						Lista de clientes registrados
 					</p>
-					<div className="py-2">
-						<CustomerListing />
-					</div>
+					<div className="py-2" />
+					<CustomerListing />
 				</div>
 
 				{/* Action List */}
-				<div className="col-span-6 px-2 py-4">
+				<div className="col-span-4 px-2 py-4">
 					<h2 className="text-lg text-right">Acciones</h2>
 					<div className="py-2"></div>
 					<div className="flex flex-wrap gap-2 w-fit ml-auto text-zinc-50">
