12ec71a91a5c32c5792e38b344ad66ce118d6950
VladWithCode <vladwithcode@gmail.com>
2023-09-30 01:40:53 -0600

    Change layout to match styles

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -1,13 +1,37 @@
+'use client';
 import ProductList from './_components/ProductList';
 import ActionsMenu from './_components/Menu/ActionsMenu';
+import { useState } from 'react';
+import Searchbar from '../_components/Searchbar';
 
 export default function products() {
-	return (
-		<div className="w-full h-page overflow-hidden">
-			{/* <ControlWrapper /> */}
-			<ActionsMenu />
+	const [search, setSearch] = useState('');
 
-			<ProductList />
+	return (
+		<div className="h-page max-w-screen-xl grid grid-cols-12 auto-rows-fr mx-auto gap-x-4 py-2">
+			{/* <ControlWrapper /> */}
+			<div className="flex flex-col col-span-7 px-4 py-2 overflow-hidden">
+				<h1 className="text-xl font-medium shrink grow-0 basis-auto">
+					Productos
+				</h1>
+				<p className="text-zinc-500 shrink grow-0 basis-auto">
+					Listado de productos
+				</p>
+				<div className="py-2" />
+				<Searchbar
+					onSearch={s => setSearch(s)}
+					placeholder="Buscar producto..."
+				/>
+				<div className="py-2" />
+				<div className="flex-auto overflow-y-auto custom-scroll-bar pr-1">
+					<ProductList search={search} />
+				</div>
+			</div>
+			<div className="col-span-5 px-4 py-2">
+				<h2 className="text-lg font-medium">Acciones</h2>
+				<div className="py-2" />
+				<ActionsMenu />
+			</div>
 		</div>
 	);
 }
