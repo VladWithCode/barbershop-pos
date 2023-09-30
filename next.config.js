8ba31197d1c672f7f3828f7c800283c743865507
VladWithCode <vladwithcode@gmail.com>
2023-09-30 16:19:55 -0600

    Fix shitty nextjs experience

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['localhost', 'api.certx-mx.org', '192.168.1.2'],
+		domains: [
+			'localhost',
+			'api.certx-mx.org',
+			'192.168.1.2',
+			'192.168.1.3',
+		],
 	},
 };
 
