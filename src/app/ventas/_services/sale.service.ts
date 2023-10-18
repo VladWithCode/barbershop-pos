8c7a501aa5b1e817aebb0c8c3e2d32194affa444
vladwithcode <vladwithcode@gmail.com>
2023-10-18 17:45:36 -0600

    Add fetching for saels

--- src/app/ventas/_services/sale.service.ts
+++ src/app/ventas/_services/sale.service.ts
@@ -4,7 +4,7 @@ import {
 	PaymentType,
 	Product,
 } from '../_stores/useCreateSaleStore';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 export type SaleData = {
 	customer: string;
@@ -16,6 +16,60 @@ export type SaleData = {
 	commission: number;
 	installment?: number;
 	next_payment_date?: string;
+};
+
+export type Sale = {
+	_id: string;
+	items: any[];
+	customer: string;
+	customer_name: string;
+	seller: string;
+	payment_type: PaymentType;
+	deposit: number;
+	deposit_date: Date;
+	installment: number;
+	paid_amount: number;
+	pending_amount: number;
+	total_amount: number;
+	next_payment_date: number;
+	last_payment_data: number;
+	payments: any[];
+	commission: number;
+	total_utility: number;
+	credit_start_date: Date;
+	credit_end_date: Date;
+	status: 'paid' | 'pending_payment' | 'over_due';
+};
+
+export async function getSale(id: string) {
+	const response = await AxiosInstance.get<Sale>('/sales/' + id);
+
+	return response.data;
+}
+
+export async function getSales({
+	search,
+}: {
+	search?: string;
+}): Promise<Sale[]> {
+	const params = new URLSearchParams();
+
+	if (search) params.append('search', search);
+
+	const response = await AxiosInstance.get<Sale[]>(
+		'/sales?' + params.toString()
+	);
+
+	return response.data;
+}
+
+export const useSales = (params?: { search?: string }) => {
+	const { search } = params || {};
+
+	return useQuery<Sale[], { message: string; error: any }>(
+		['sales', 'get', params],
+		() => getSales({ search })
+	);
 };
 
 export async function createSale(saleData: SaleData) {
