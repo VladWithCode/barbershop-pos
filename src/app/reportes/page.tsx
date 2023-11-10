bd10eba7dd9197e7ddae0aba50de606fa3871064
vladwithcode <vladwithcode@gmail.com>
2023-11-10 12:34:31 -0600

    Add Reports page

---
+++ src/app/reportes/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from '../_components/Page';
+
+export default function Reportes() {
+	return (
+		<Page>
+			<div className="col-span-6 px-4 py-2">
+				<h1 className="text-xl">Reportes</h1>
+			</div>
+		</Page>
+	);
+}
