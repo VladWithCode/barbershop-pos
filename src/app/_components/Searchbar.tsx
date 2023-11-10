eadc7f968c2afa72b86034c461953bb39d1d4215
vladwithcode <vladwithcode@gmail.com>
2023-11-10 12:33:11 -0600

    Change use provided name or default one

--- src/app/_components/Searchbar.tsx
+++ src/app/_components/Searchbar.tsx
@@ -4,10 +4,15 @@ import React, { useState } from 'react';
 
 export type SearchbarProps = {
 	onSearch: (query: string) => void;
+	name?: string;
 	placeholder?: string;
 };
 
-export default function Searchbar({ onSearch, placeholder }: SearchbarProps) {
+export default function Searchbar({
+	onSearch,
+	placeholder,
+	name,
+}: SearchbarProps) {
 	const [searchValue, setSearchValue] = useState('');
 
 	useDebounce(() => onSearch(searchValue), 700, [searchValue]);
@@ -16,12 +21,12 @@ export default function Searchbar({ onSearch, placeholder }: SearchbarProps) {
 		<div className="flex items-center h-10">
 			<ControlledInput
 				className="w-full peer h-full"
-				name="search-custome"
+				name={name || 'search-bar'}
 				value={searchValue}
 				onChange={e => setSearchValue(e.target.value)}
 				placeholder={placeholder ?? 'Buscar...'}
 			/>
-			<div className="flex items-center justify-center h-full w-1/12 border-2 border-transparent -ml-1 peer-focus:bg-zinc-800 peer-focus:border-zinc-50 rounded-e cursor-pointer">
+			<div className="flex items-center justify-center h-full w-1/12 border-2 border-transparent -ml-1 peer-focus:bg-zinc-800 peer-focus:border-zinc-50 peer-focus:text-zinc-50 rounded-e cursor-pointer">
 				<svg className="w-8 h-8 fill-current">
 					<use href="/sprites.svg#search"></use>
 				</svg>
